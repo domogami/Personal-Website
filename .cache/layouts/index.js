1cddce6f73246da838e4158016f3e999e79fd0a2
Dominick <d3lee@ucsd.edu>
2020-08-02 13:28:14 -0700

    built

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/Users/domogami/Documents/GitHub/Personal-Website/src/layouts/index.js"
+  import data from "/Users/domogami/Documents/GitHub/Personal-Website/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
