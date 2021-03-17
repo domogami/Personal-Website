c3003980f499d1f1e1bddf93f7794b083631b244
Dominick <d3lee@ucsd.edu>
2021-03-17 01:50:15 -0700

    ☕️ Fun Cursor

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,7 @@ import IEEEMockUp from "../images/IEEEMockUp.png"
 import SnowStorm from 'react-snowstorm';
 var ReactRotatingText = require("react-rotating-text");
 
+import Cursor from "../components/cursor"
 function renderSnow() {
   if (typeof window !== 'undefined') {
     return (
@@ -23,43 +24,85 @@ function renderSnow() {
     )
   }
 }
+function renderCursor() {
+  const cursor = new Cursor();
+}
 
 const IndexPage = () => (
   <div>
+    {/* <div class="cb-demo">
+      <div class="cb-demo-content">
+        <div class="cb-demo-container">
+          <div class="cb-demo-items">
+            <div class="cb-demo-row">
+              <div class="cb-demo-item" data-cursor="-color-red">
+                <div class="cb-demo-item-title">Set Color</div>
+                <div class="cb-demo-item-text" data-cursor="-color-green">Any color via CSS class with <b>.setState('colorClass')</b> or <b>[data-cursor="colorClass"</b>] attribute</div>
+              </div>
+              <div class="cb-demo-item" data-cursor-text="Hello!">
+                <div class="cb-demo-item-title">Set Text</div>
+                <div class="cb-demo-item-text">Set text with <b>.setText('Text string')</b> or <b>[data-cursor-text="Text string"</b>] attribute</div>
+              </div>
+              <div class="cb-demo-item" data-cursor="-opaque">
+                <div class="cb-demo-item-title">Exclusion mode</div>
+                <div class="cb-demo-item-text">Exclusion or opaque mode with <b>.setState('-opaque')</b> or <b>[data-cursor="-opaque"</b>] attribute</div>
+              </div>
+            </div>
+            <div class="cb-demo-row">
+              <div class="cb-demo-item" data-cursor="-lg">
+                <div class="cb-demo-item-title">Set size</div>
+                <div class="cb-demo-item-text">Any size via CSS class with <b>.setState('sizeClass')</b> or <b>[data-cursor="sizeClass"</b>] attribute</div>
+              </div>
+              <div class="cb-demo-item" data-cursor="-exclusion -lg" data-cursor-stick="#cursor-stick-area">
+                <div class="cb-demo-item-title" id="cursor-stick-area">Stick</div>
+                <div class="cb-demo-item-text">Set sticky cursor with <b>.setStick(element)</b> or <b>[data-cursor-stick="selector"</b>] attribute</div>
+              </div>
+              <div class="cb-demo-item" data-magnetic data-cursor="-opaque">
+                <div class="cb-demo-item-title">Magnetic</div>
+                <div class="cb-demo-item-text">Magnetic via [data-magnetic] attribute and opaque style cursor</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> */}
     <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-    <div className="Main">
-      {/* {renderSnow()} */}
-      <div className="MainGroup">
-        <img src={Profile} alt="" className="profile-img" />
-        <p>Hey, I'm</p>
-        <h1>Dominick Lee</h1>
-        {/* A JSX comment */}
-        <h3>
-          <ReactRotatingText
-            items={["Student", "Developer", "Leader", "Designer"]}
-          />
-        </h3>
-
-        <a href={Resume} download="Resume">
-          Download Résumé{" "}
-        </a>
-        <svg width="100%" height="172" viewBox="0 0 100% 172" fill="none">
-          <path fill="#EFEEEE">
-            <animate
-              repeatCount="indefinite"
-              fill="freeze"
-              attributeName="d"
-              dur="10s"
-              values="M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z;
-
-          M0 86.3149C316 86.315 444 159.155 884 51.1554C1324 -56.8446 1320.29 34.1214 1538 70.4063C1814 116.407 2156 188.408 2560 86.315V232.317L0 232.316V86.3149Z;
-
-          M0 53.6584C158 11.0001 213 0 363 0C513 0 855.555 115.001 1154 115.001C1440 115.001 1626 -38.0004 2560 53.6585V199.66L0 199.66V53.6584Z;
-
-          M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z;"
+    <div class="cb-demo-item" data-cursor="-color-white">
+    
+      <div className="Main">
+        {/* {renderSnow()} */}
+        {renderCursor()}
+        <div className="MainGroup">
+          <img src={Profile} alt="" className="profile-img" />
+          <p>Hey, I'm</p>
+          <h1>Dominick Lee</h1>
+          <h3>
+            <ReactRotatingText
+              items={["Student", "Developer", "Leader", "Designer"]}
             />
-          </path>
-        </svg>
+          </h3>
+          <a href={Resume} download="Resume">
+            Download Résumé{" "}
+          </a>
+          <div class="cb-demo-item" data-cursor="-color-black">
+          <svg width="100%" height="172" viewBox="0 0 100% 172" fill="none">
+            <path fill="#EFEEEE">
+              <animate
+                repeatCount="indefinite"
+                fill="freeze"
+                attributeName="d"
+                dur="10s"
+                values="M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z;
+
+            M0 86.3149C316 86.315 444 159.155 884 51.1554C1324 -56.8446 1320.29 34.1214 1538 70.4063C1814 116.407 2156 188.408 2560 86.315V232.317L0 232.316V86.3149Z;
+
+            M0 53.6584C158 11.0001 213 0 363 0C513 0 855.555 115.001 1154 115.001C1440 115.001 1626 -38.0004 2560 53.6585V199.66L0 199.66V53.6584Z;
+
+            M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z;"
+              />
+            </path>
+          </svg></div>
+        </div>
       </div>
     </div>
     <div id="Projects">
@@ -73,7 +116,9 @@ const IndexPage = () => (
       data-aos-duration="11000"
     >
       <div className="Projects">
-        <img src={IEEEMockUp} className="enlightenUI" />
+      <div class="cb-demo-item" data-cursor-text="IEEE Site!">
+        <a href="https://ieeeucsd.org"><img src={IEEEMockUp} className="enlightenUI" /></a>
+      </div>
         <div className="ProjectDescription">
           <ProjectCard
             title="IEEE Website"
@@ -97,7 +142,9 @@ const IndexPage = () => (
             text="Office Inc. is an iOS game coming soon to the App Store. You are in the shoes of an averege office employee and make decisions leading you closer down the path of your choosing. You can discover the dark secrets of the company or find some office romance."
           />
         </div>
-        <img src={Office} className="enlightenUI" />
+        <div class="cb-demo-item" data-cursor-text="Git Code!">
+          <a href="https://github.com/domogami/Office-Simulator"><img src={Office} className="enlightenUI" /></a>
+        </div>
       </div>
     </div>
     <div
@@ -107,7 +154,10 @@ const IndexPage = () => (
       data-aos-duration="11000"
     >
       <div className="Projects">
-        <img src={Time} className="enlightenUI" />
+        
+        <div class="cb-demo-item" data-cursor-text="App Store!">
+          <a href="https://apps.apple.com/us/app/timezone-hero/id1521513208"><img src={Time} className="enlightenUI" /></a>
+        </div>
         <div className="ProjectDescription">
           <ProjectCard
             title="TimeZone Hero"
@@ -131,7 +181,9 @@ const IndexPage = () => (
             text="enLIGHTEN is an IEEE First Place project that allows users to not only control light switches from an app but also enable a machine learning camera to turn on only the lights in the area you are currently in. The project earned first place at UCSD's IEEE Branch QP Competiton and made use of SwiftUI, Python, ESPs, and a Raspberry pi"
           />
         </div>
-        <img src={Light} className="enlightenUI" />
+        <div class="cb-demo-item" data-cursor-text="Git Code!">
+          <img src={Light} className="enlightenUI" />
+        </div>
       </div>
     </div>
 
